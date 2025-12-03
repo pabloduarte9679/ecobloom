ce2e1f5139ad6c562ea1111a5cf5fbbff22fcecc
pablo <92185570+pabloduarte9679@users.noreply.github.com>
2025-12-02 21:55:40 -0600

    Update luz.php

--- luz.php
+++ luz.php
@@ -1,10 +1,8 @@
 <?php
 
-// Listen for broadcast
 $udp = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
 socket_bind($udp, "0.0.0.0", 5005);
 
-// Receive the Pico IP
 socket_recvfrom($udp, $buf, 1024, 0, $from, $port);
 socket_close($udp);
 
@@ -14,13 +12,11 @@ if (strpos($buf, "PICO_IP:") === 0) {
     die("Invalid discovery packet received");
 }
 
-echo "Pico discovered at: $pico_ip\n";
+echo "rpi at: $pico_ip\n";
 
-// Connect to Pico TCP server
 $tcp = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 socket_connect($tcp, $pico_ip, 8080);
 
-// Now you can send your commands
 $estado = $_GET['estado'] ?? '';
 
 if ($estado == 'on') socket_write($tcp, "1");
